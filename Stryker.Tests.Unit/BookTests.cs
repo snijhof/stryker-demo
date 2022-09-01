d7d8973daf48a8a1ca9be1de5fbc466e97a998a6
DESKTOP-IV2654P\Sjoerd Nijhof <sjoerd@terraindex.com>
2022-09-01 16:56:25 +0200

    .

---
+++ Stryker.Tests.Unit/BookTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Test;
+using Xunit;
+
+namespace Stryker.Tests.Unit;
+
+public class BookTests
+{
+    [Fact]
+    public void Can_be_able_to()
+    {
+        // Arrange
+        var sut = new Book(100, "sjoerd");
+
+        // Act
+        sut.TurnPage();
+
+        // Assert
+        sut.CurrentPage.Should().Be(1);
+    }
+
+    [Fact]
+    public void Can_be_able_to1()
+    {
+        // Arrange
+        var sut = new Book(100, "sjoerd");
+
+        // Act
+        sut.TurnPage();
+        sut.TurnBackPage();
+
+        // Assert
+        sut.CurrentPage.Should().Be(0);
+    }
+}
---
+++ Test/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace Test;
+
+public class Book
+{
+    private readonly int pages;
+    private readonly string writer;
+
+    public Book(int pages, string writer)
+    {
+        this.pages = pages;
+        this.writer = writer;
+    }
+
+    public int CurrentPage { get; private set; } = 0;
+
+    public void TurnPage()
+    {
+        CurrentPage++;
+    }
+
+    public void TurnBackPage()
+    {
+        CurrentPage--;
+    }
+}
